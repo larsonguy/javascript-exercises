7b595806b7118b773a45d35204cb4ac9b4a64dc7
Lawrence Bellomo <larsonguy@gmail.com>
2022-09-28 00:01:24 -0400

    Add solutions for exercises 01-04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = ''
+    
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let count = 0; count < num; count++) {
+            result = result + string;
+        }    
+        return result;
+    }
 
+    
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    // take the string and pop out the first letter
+    // append that letter to the end of the newString
+    //  
+    
+    let strLength = string.length;
+    let newString = ''
+    for (let i = 0; i <= strLength; i++ ) {
+        let strIndex = string.charAt(strLength - i);
+        newString = newString + strIndex;
+    }
 
+    return newString;
 };
 
 // Do not edit below this line
