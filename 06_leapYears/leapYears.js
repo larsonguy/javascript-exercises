e1e137186910d8e505b96e207fead7e531df4464
Lawrence Bellomo <larsonguy@gmail.com>
2022-10-02 22:10:31 -0400

    Add solutions for 06 and 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+// take year input
+// if year % 4 == 0 enter nested if 
+// nested if: if it is divisible by 400 and 100, return true
+
+    if (year % 4 == 0) {
+        if ((year % 100 == 0) && (year % 400 == 0)) {
+            return true;
+        } else if (year % 100 !== 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 
 };
 
